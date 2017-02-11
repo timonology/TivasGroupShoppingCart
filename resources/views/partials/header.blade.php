1bad5f7b819059b624a8f79c109f2f6f007adb00
= <=>
2017-02-11 13:58:02 +0100

    Changed the Header name

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="{{ route('product.index') }}">Brand</a>
+            <a class="navbar-brand" href="{{ route('product.index') }}">Tivas Group</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->

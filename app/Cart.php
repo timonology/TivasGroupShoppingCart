6a6b7dd72a68b652b95e017c24087332a897168b
= <=>
2017-02-11 18:42:03 +0100

    Show count of all Items

--- app/Cart.php
+++ app/Cart.php
@@ -30,7 +30,7 @@ class Cart
     {
         $storedItem = ['qty' => 0, 'price' => $item->price, 'item' => $item];
         if($this->items){
-            if(array_key_exists($id, $item))
+            if(array_key_exists($id, $this->items))
             {
                 $storedItem = $this->items[$id];
             }
